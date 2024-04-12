0f5009bf137ede0f91311ef4d571810a206228b2
RaulGeronimo400 <geronimoraul400@gmail.com>
2024-04-12 15:13:15 -0600

    Agregar archivos de proyecto.

---
+++ BL/Usuario.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        public static Dictionary<string, object> Add(ML.Usuario usuario)
+        {
+            string exepcion = "";
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Resultado", false }, { "Excepcion", exepcion } };
+
+            try
+            {
+                using (DL.SGomezSDIPrueba1Entities context = new DL.SGomezSDIPrueba1Entities())
+                {
+                    int filasAfectadas = context.UsuarioAdd(usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.Edad);
+
+                    if (filasAfectadas > 0)
+                    {
+                        diccionario["Resultado"] = true;
+                        diccionario["Usuario"] = usuario;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }catch (Exception ex) 
+            {
+                diccionario["Resultado"] = false;
+                diccionario["Excepcion"] = ex.Message;
+            }
+            return diccionario;
+        }
+        public static Dictionary<string, object> GetAll()
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            string exepcion = "";
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Resultado", false }, { "Excepcion", exepcion }, { "Usuario", usuario } };
+            try
+            {
+                using (DL.SGomezSDIPrueba1Entities context = new DL.SGomezSDIPrueba1Entities())
+                {
+                    var registros = context.UsuarioGetAll().ToList();
+
+                    if( registros != null )
+                    {
+                        usuario.Usuarios = new List<object>();
+                        foreach( var registro in registros )
+                        {
+                            ML.Usuario user = new ML.Usuario();
+
+                            user.IdUsuario = registro.IdUsuario;
+                            user.Nombre = registro.Nombre;
+                            user.ApellidoPaterno = registro.ApellidoPaterno;
+                            user.ApellidoMaterno = registro.ApellidoMaterno;
+                            user.Edad = (int)registro.Edad;
+
+                            usuario.Usuarios.Add(user);
+                        }
+                        diccionario["Resultado"] = true;
+                        diccionario["Usuario"] = usuario;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }catch(Exception ex) 
+            {
+                diccionario["Excepcion"] = ex.Message;
+                diccionario["Resultado"] = false;
+            }
+            return diccionario;
+        }
+        public static Dictionary<string, object> Update(ML.Usuario usuario)
+        {
+            string exepcion = "";
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Excepcion", exepcion }, { "Resultado", false } };
+
+            try
+            {
+                using (DL.SGomezSDIPrueba1Entities context = new DL.SGomezSDIPrueba1Entities())
+                {
+                    int filasAfectadas = context.UsuarioUpdate(usuario.IdUsuario, usuario.Nombre, usuario.ApellidoPaterno, usuario.ApellidoMaterno, usuario.Edad);
+                    if (filasAfectadas > 0)
+                    {
+                        diccionario["Resultado"] = true;
+                        diccionario["Usuario"] = usuario;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                diccionario["Resultado"] = false;
+                diccionario["Excepcion"] = ex.Message;
+            }
+            return diccionario;
+        }
+        public static Dictionary<string, object> GetById(int IdUsuario)
+        {
+            ML.Usuario usuario= new ML.Usuario();
+            string exepcion = "";
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Usuario", usuario }, { "Resultado", false }, { "Excepcion", exepcion } };
+
+            try
+            {
+                using (DL.SGomezSDIPrueba1Entities context = new DL.SGomezSDIPrueba1Entities())
+                {
+                    var registro = context.UsuarioGetById(IdUsuario).SingleOrDefault();
+                    if (registro != null)
+                    {
+                        diccionario["Resultado"] = true;
+                        diccionario["Usuario"] = usuario;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                diccionario["Resultado"] = false;
+                diccionario["Excepcion"] = ex.Message;
+            }
+            return diccionario;
+        }
+        public static Dictionary<string, object> Delete(int IdUsuaro)
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            string exepcion = "";
+            Dictionary<string, object> diccionario = new Dictionary<string, object> { { "Excepcion", exepcion }, { "Resultado", false }, { "Usuario", usuario } };
+
+            try
+            {
+                using (DL.SGomezSDIPrueba1Entities context = new DL.SGomezSDIPrueba1Entities())
+                {
+                    int filasAfectadas = context.UsuarioDelete(IdUsuaro);
+                    if (filasAfectadas > 0)
+                    {
+                        diccionario["Resultado"] = true;
+                        diccionario["Usuario"] = usuario;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                diccionario["Resultado"] = false;
+                diccionario["Excepcion"] = ex.Message;
+            }
+            return diccionario;
+        }
+    }
+}
---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            Dictionary<string, object> objeto = BL.Usuario.GetAll();
+            bool resultado = (bool)objeto["Resultado"];
+            if (resultado)
+            {
+                ML.Usuario usuario = new ML.Usuario();
+                usuario = (ML.Usuario)objeto["Usuario"];
+                return View(usuario);
+            }
+            else
+            {
+                string excepcion = (string)objeto["Excepcion"];
+                ViewBag.Mensaje = $"Ocurrio un error {excepcion}";
+                return View(excepcion);
+            }
+        }
+        [HttpGet]
+        public ActionResult Form(int? IdUsuario)
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            if (IdUsuario != null)
+            {
+                Dictionary<string, object> result = BL.Usuario.GetById(IdUsuario.Value);
+                bool resultado = (bool)result["Resultado"];
+            }
+            return View(usuario);
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Usuario usuario)
+        {
+            Dictionary<string, object> result = BL.Usuario.Add(usuario);
+            bool resultado = (bool)result["Resultado"];
+            if (resultado)
+            {
+                ViewBag.Mensaje = "El Usuario ha sido insertado";
+                return PartialView("Modal");
+            }
+            else
+            {           
+                string exepcion = (string)result["Exepcion"];
+                ViewBag.Mensaje = "El usuario no se pudo registrar " + exepcion;
+                return PartialView("Modal");
+            }
+        }
+        public ActionResult Delete(int? IdUsuario)
+        {
+            ML.Usuario usuario = new ML.Usuario();
+            Dictionary<string, object> result = BL.Usuario.Delete(IdUsuario.Value);
+            bool resultado = (bool)result["Resultado"];
+
+            if (resultado == true)
+            {
+                ViewBag.Mensaje = "El Usuario ha sido eliminado";
+                return PartialView("Modal");
+            }
+            else
+            {
+                ViewBag.Mensaje = "ERROR! El usuario no se elimino "; 
+                return PartialView("Modal");
+            }
+        }
+    }
+}
